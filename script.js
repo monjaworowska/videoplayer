22e90148fbef64df11d1a4e18858a6116e86e48e
Monika Jaworowska <monika.jaworowska@wp.pl>
2020-05-25 21:48:59 +0200

    Methods: updateLoadedBar, updatePlayBar, rewind added

--- script.js
+++ script.js
@@ -5,6 +5,10 @@
 		this.video = videoContainer.querySelector("video");
 		this.playPause = videoContainer.querySelector(".playPause");
 
+		this.progressBar = videoContainer.querySelector(".progressBar");
+		this.playBar = videoContainer.querySelector(".playBar");
+		this.loadedBar = videoContainer.querySelector(".loadedBar");
+
 		this.assignEventListeners();
 
 	}
@@ -14,7 +18,13 @@
 		this.playPause.onclick = this.play.bind(this);
 		this.video.onended = this.reset.bind(this);
 
+		this.video.onprogress = this.updateLoadedBar.bind(this);
+		this.video.addEventListener("timeupdate", this.updatePlayBar.bind(this), false);
+		this.progressBar.onclick = this.rewind.bind(this);
+
 	};
+
+	/* Play, reset */
 
 	Player.prototype.play = function(e){
 		if(this.video.paused){
@@ -26,12 +36,34 @@
 			e.target.classList.remove("showPause");
 			e.target.classList.add("showPlay");
 		}
-	}
+	};
 
 	Player.prototype.reset = function(){
 		this.playPause.classList.remove("showPause");
 		this.playPause.classList.add("showPlay");
-	}
+	};
+
+	/* UpdateLoadedBar, updatePlayBar, rewind */
+
+	Player.prototype.updateLoadedBar = function(){
+		if(this.video.buffered.length > 0){
+			this.loadedBar.style.width = (this.video.buffered.end(0) / this.video.duration) * 100 + "%";
+		}
+	};
+
+	Player.prototype.updatePlayBar = function(){
+		this.playBar.style.width = (this.video.currentTime / this.video.duration) * 100 + "%";
+	};
+
+	Player.prototype.rewind = function(e){
+		var barEdge = this.progressBar.getBoundingClientRect().left,
+			clickedX = e.pageX,
+			barClickedX = clickedX - barEdge,
+			currentTime = this.video.duration * (barClickedX / this.progressBar.offsetWidth);
+		this.video.currentTime = currentTime;
+	};
+
+
 
 	new Player(document.querySelector("#videoPlayer1"));
 
